f1c76c77956bb52c7860d710e994db593aa049a5
chickenpwny <35206540+chickenpwny@users.noreply.github.com>
2025-04-08 10:33:28 -0400

    install automatic yes to installs

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update packages
-sudo apt update 
+sudo apt update --y
 
 # Install PostgreSQL
-sudo apt-get install postgresql
+sudo apt-get install postgresql --y
 
 # Create PostgreSQL user and database
 sudo -u postgres psql -c "CREATE USER postgres;"
@@ -18,11 +18,11 @@ sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '$postgres_password'
 sudo -u postgres psql -c "CREATE DATABASE test;"
 
 # Install nmap
-sudo apt-get install nmap
+sudo apt-get install nmap --y
 
 # Install Python3 and pip
-sudo apt install python3 python3-dev
-sudo apt install python3-pip
+sudo apt install python3 python3-dev --y
+sudo apt install python3-pip --y
 
 # Install Python requirements
 pip3 install -r ./ ego/requirements.txt
@@ -31,7 +31,7 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 
 # Update packages again
-sudo apt-get update
+sudo apt-get update --y
 
 # Download and install Go
 wget https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
@@ -56,5 +56,5 @@ sed -i "s/expected_secret_code = .*/expected_secret_code = '$new_secret_code'/" 
 
 python3 manage.py collectstatic
 
-sudo apt-get install mailutils
-sudo apt-get install postfix dovecot-imapd dovecot-pop3d
+sudo apt-get install mailutils --y
+sudo apt-get install postfix dovecot-imapd dovecot-pop3d --y
