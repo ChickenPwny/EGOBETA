f4355b08bba8a57ed2767d458def169e46063d89
chickenpwny <charles.mcgowen@politoinc.com>
2025-05-27 22:16:08 -0400

    agent

--- AgentInstall.sh
+++ AgentInstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Create a directory for static files
 mkdir STATICFILES_DIRS
 
 # Update packages
@@ -8,3 +9,21 @@ sudo apt update -y
 # Install nmap
 sudo apt-get install nmap -y
 
+# Install Nuclei
+echo "Installing Nuclei..."
+curl -s https://api.github.com/repos/projectdiscovery/nuclei/releases/latest | \
+grep "browser_download_url.*nuclei-linux-amd64.zip" | \
+cut -d '"' -f 4 | \
+wget -qi -
+
+# Unzip the downloaded file
+unzip nuclei-linux-amd64.zip
+
+# Move the binary to /usr/local/bin for global access
+sudo mv nuclei /usr/local/bin/
+
+# Clean up the zip file
+rm nuclei-linux-amd64.zip
+
+# Verify installation
+nuclei -version
